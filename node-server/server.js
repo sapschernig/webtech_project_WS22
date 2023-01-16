91f70ec8be4a7336c1a4f0626bccd984804b1b58
sapschernig <72254885+sapschernig@users.noreply.github.com>
2023-01-16 20:36:01 +0100

    functionality to check for database

--- node-server/server.js
+++ node-server/server.js
@@ -19,3 +19,33 @@ app.get("/:customText", (req, res) => {
 let port = 3000;
 app.listen(port);
 console.log("Server running at: http://localhost:" + port);
+
+const { Client } = require('pg');
+
+// Connect to PostgreSQL
+const client = new Client({
+    user: 'your_username',
+    host: 'your_host',
+    database: 'movie_db',
+    password: 'your_password',
+    port: 5432,
+});
+client.connect();
+
+// Check if the movie table already exists
+client.query('SELECT * FROM information_schema.tables WHERE table_name = $1', ['movie'], (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    if (!result.rows[0]) {
+        // If the table does not exist, create it
+        client.query(`
+            CREATE TABLE movie (
+                id SERIAL PRIMARY KEY,
+                title VARCHAR(255) NOT NULL,
+                genre VARCHAR(255) NOT NULL,
+                rating DECIMAL(3,1) NOT NULL
+            );
+        `)
+    }
+});
