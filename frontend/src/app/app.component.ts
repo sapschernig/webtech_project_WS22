0731f4700b1a7c9189a8ee892386994a5f3e0949
matheb1 <101284481+matheb1@users.noreply.github.com>
2023-01-21 17:01:55 +0100

    Test verbindung

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppServiceService } from './app-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'webtech-projekt';
+
+  constructor(private service : AppServiceService) {
+
+  }
+
+  ngOnInit() {
+
+    this.getDataFromAPI();
+
+  }
+
+  getDataFromAPI(){
+    this.service.getTest().subscribe((response) => {
+      console.log('Response from API is: ' + response);
+    }, (error) => {
+      console.log('Error: ' + error);
+      
+    })
+
+
+  }
+
+
 }
