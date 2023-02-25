6b47bd51148606bd36de76e2f27adca89c55d97a
sapschernig <72254885+sapschernig@users.noreply.github.com>
2023-02-25 12:36:18 +0100

    display movies from server primitively

--- frontend/src/app/moviecard/moviecard.component.ts
+++ frontend/src/app/moviecard/moviecard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 
+
 @Component({
   selector: 'app-moviecard',
   templateUrl: './moviecard.component.html',
@@ -9,16 +10,16 @@ import { HttpClient } from '@angular/common/http';
 })
 
 export class MoviecardComponent implements OnInit{
-  movies: any[];
+  movies: any[]=[];
 
   constructor(private http: HttpClient) { 
     this.movies = []; //initialize movie property
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.http.get<any[]>('/api/movies')
-      .subscribe(data => {
-        this.movies = data;
+      .subscribe(movies => {
+        this.movies = movies;
       }, error => {
         console.error(error);
       });
