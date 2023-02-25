afaac0db61f9b6d54d50f72f6666afcfae5e64db
sapschernig <72254885+sapschernig@users.noreply.github.com>
2023-02-25 20:37:44 +0100

    wrote logic for registerUser()

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -12,10 +12,10 @@ import { Customer } from '../interfaces/customer';
 export class RegisterComponent implements OnInit{
 
   title = 'Register';
-
+  errorMessage = '';
   registerForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
   
   
   ngOnInit(): void {
@@ -32,9 +32,30 @@ export class RegisterComponent implements OnInit{
 }
 
 
-  registerUser(){
-    console.warn(this.registerForm.value)
-  }
+registerUser() {
+  // Check if user already exists
+  this.http.get<boolean>(`/api/checkUserExists/${this.registerForm.value.email}`)
+    .subscribe((userExists) => {
+      if (userExists) {
+        // User already exists, show error message
+        this.errorMessage = 'User already exists';
+      } else {
+        // User does not exist, create new user
+        this.http.post('/api/createUser', this.registerForm.value)
+          .subscribe(() => {
+            // User created successfully, navigate to login page or show success message
+          }, (error) => {
+            // Handle error
+            console.error(error);
+            this.errorMessage = 'An error occurred while creating the user';
+          });
+      }
+    }, (error) => {
+      // Handle error
+      console.error(error);
+      this.errorMessage = 'An error occurred while checking if the user exists';
+    });
+}  
 
   get email(){
     return this.registerForm.get('email');
