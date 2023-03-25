86eacc4d7b2db87cb6bbd1cb8d784465c964d5c0
sapschernig <72254885+sapschernig@users.noreply.github.com>
2023-03-25 14:46:35 +0100

    npm install moment

--- frontend/src/app/services/movieService.ts
+++ frontend/src/app/services/movieService.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { map, mergeMap, Observable } from 'rxjs';
+import { forkJoin, map, mergeMap, Observable } from 'rxjs';
+import * as moment from 'moment';
+
 
 @Injectable({
   providedIn: 'root'
@@ -8,62 +10,72 @@ import { map, mergeMap, Observable } from 'rxjs';
 export class MovieService {
     constructor(private http: HttpClient) { }
 
-    getWeeklyShowtimes(sortField: string, sortDirection: string, filterMovieTitle?: string): Observable<any[]> {
+    getWeeklyShowtimes(sortField: string, sortDirection: string): Observable<any[]> {
+        // set up the query parameters for the API call
         let params = new HttpParams();
         params = params.append('start_date', moment().startOf('week').format('YYYY-MM-DD'));
         params = params.append('end_date', moment().endOf('week').format('YYYY-MM-DD'));
       
-       
-
-
-        if (filterMovieTitle) {
-          params = params.append('movie_title', filterMovieTitle);
-        }
-      
         return this.http.get<any[]>('api/showtimes', { params }).pipe(
-            mergeMap((showtimes: any[]) : Observable<any[]> => {
-              // get an array of movie ids from the showtimes
-              const movieIds = showtimes.map(showtime => showtime.movie_id);
-          
-              // retrieve the movies that match the ids
-              return this.http.get<any[]>('api/movies', {
-                params: new HttpParams().set('id', movieIds.join())
-              }).pipe(
-                map(movies => {
-                  // combine the showtimes and movies data
-                  return showtimes.map(showtime => {
-                    const movie = movies.find(movie => movie.id === showtime.movie_id);
-                    return {
-                      id: showtime.id,
-                      movie_id: showtime.movie_id,
-                      theater_id: showtime.theater_id,
-                      start_time: showtime.start_time,
-                      date: showtime.date,
-                      movie_title: movie ? movie.title : null,
-                      theater_name: showtime.theater && showtime.theater.name
-                    };
-                  });
-                })
-              );
-            }),
-            map(data => {
-              // sort the data based on the sort field and direction
-              data.sort((a, b) => {
-                const fieldA = a[sortField];
-                const fieldB = b[sortField];
-                if (fieldA < fieldB) {
-                  return sortDirection === 'asc' ? -1 : 1;
-                } else if (fieldA > fieldB) {
-                  return sortDirection === 'asc' ? 1 : -1;
-                } else {
-                  return 0;
-                }
-              });
-          
-              return data;
-            })
-          );
+          mergeMap(showtimes => {
+            // get an array of movie ids from the showtimes
+            const movieIds = showtimes.map(showtime => showtime.movie_id);
+      
+            // retrieve the movies that match the ids
+            const movies$ = this.http.get<any[]>('api/movies', {
+              params: new HttpParams().set('id', movieIds.join())
+            });
+      
+            // get an array of theater ids from the showtimes
+            const theaterIds = showtimes.map(showtime => showtime.theater_id);
+      
+            // retrieve the theaters that match the ids
+            const theaters$ = this.http.get<any[]>('api/theater', {
+              params: new HttpParams().set('id', theaterIds.join())
+            });
+      
+            return forkJoin([movies$, theaters$]).pipe(
+              map(([movies, theaters]) => {
+                // combine the showtimes, movies, and theaters data
+                return showtimes.map(showtime => {
+                  const movie = movies.find(movie => movie.id === showtime.movie_id);
+                  const theater = theaters.find(theater => theater.id === showtime.theater_id);
+                  return {
+                    id: showtime.id,
+                    movie_id: showtime.movie_id,
+                    theater_id: showtime.theater_id,
+                    start_time: showtime.start_time,
+                    date: showtime.date,
+                    movie_title: movie ? movie.title : null,
+                    theater_name: theater ? theater.name : null
+                  };
+                });
+              })
+            );
+          }),
+          map(data => {
+            // sort the data based on the sort field and direction
+            data.sort((a, b) => {
+              const fieldA = a[sortField];
+              const fieldB = b[sortField];
+              if (fieldA < fieldB) {
+                return sortDirection === 'asc' ? -1 : 1;
+              } else if (fieldA > fieldB) {
+                return sortDirection === 'asc' ? 1 : -1;
+              } else {
+                return 0;
+              }
+            });
+      
+            return data;
+          })
+        );
+            
           
       }
     
+}
+
+function moment() {
+    throw new Error('Function not implemented.');
 }
