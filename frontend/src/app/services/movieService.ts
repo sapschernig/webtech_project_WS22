4afb08d483bd26f0a9da9a380c2c6d301b371332
sapschernig <72254885+sapschernig@users.noreply.github.com>
2023-03-27 22:04:59 +0200

    update movieService and create movie interface

--- frontend/src/app/services/movieService.ts
+++ frontend/src/app/services/movieService.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { forkJoin, map, mergeMap, Observable } from 'rxjs';
+import { Movie } from '../interfaces/movie';
 
 
 @Injectable({
@@ -79,6 +80,10 @@ export class MovieService {
       getMovieDuration(movieId: number) {
         return this.http.get<number>(`api/movies/${movieId}/duration`);
       }
+      getMovieById(id: number): Observable<Movie> {
+        return this.http.get<Movie>(`api/movies/${id}`);
+      }
+      
 }
 
 function moment() {
