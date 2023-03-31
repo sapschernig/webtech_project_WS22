19290b4d742434f78ed58730d66b8e230e3ce741
matheb1 <101284481+matheb1@users.noreply.github.com>
2023-03-31 20:40:48 +0200

    ticket stornieren

---
+++ frontend/src/app/interfaces/movie.ts
@@ -0,0 +1,10 @@
+export interface Movie {
+    id: number;
+    title: string;
+    release_date: Date;
+    duration: number;
+    genre: string;
+    description: string;
+    age_restriction: number;
+    picture: string;
+  }
